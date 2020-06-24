3c646603c34b6863b85d8f5f725c372b7547a70b
BenjaminTheDeveloper <39067259+BenjaminTheDeveloper@users.noreply.github.com>
2020-06-23 18:50:07 -0600

    Added logic, unsure if this is the right way

--- WorkFlowEngine/WorkFlowEngine/Program.cs
+++ WorkFlowEngine/WorkFlowEngine/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace WorkFlowEngine
 {
@@ -6,7 +7,104 @@ namespace WorkFlowEngine
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("WorkFlow Engine");
+
+            WorkFlow workFlow = new WorkFlow(new Video(), new CloudStorage(), new WebService(), new Processing());
+
+            workFlow.Run();
+
+
+
+
+            Console.ReadKey();
         }
     }
+
+
+
+
+        interface IActivity<T>
+        {
+        public void Execute(); 
+        }
+
+
+
+
+    public class WorkFlow
+    {
+        Video video;
+        CloudStorage cloudStorage;
+        WebService webService;
+        Processing processing; 
+
+        public WorkFlow(Video video, CloudStorage cloudStorage, WebService webService, Processing processing) {
+            this.video = video;
+            this.cloudStorage = cloudStorage;
+            this.webService = webService;
+            this.processing = processing;
+        
+        
+        
+        }
+
+        public void Run()
+        {
+            video.Execute();
+            cloudStorage.Execute();
+            webService.Execute();
+            processing.Execute();
+        }
+    }
+    public class Video: IActivity<Video>
+    {
+        public Video() { }
+
+        public void Execute()
+        {
+            Console.WriteLine("Uploading Video..."); 
+        }
+    }
+    public class CloudStorage : IActivity<CloudStorage>
+    {      
+
+
+        public CloudStorage()
+        {
+
+        }
+        public void Execute()
+        {
+            Console.WriteLine("Calling Web Service"); 
+        }
+    }
+
+    public class WebService : IActivity<WebService>
+    {
+
+
+
+
+
+        public WebService() { }
+
+        public void Execute()
+        {
+            Console.WriteLine("Sending email to owner.");
+        }
+
+    
+    }
+
+
+    public class Processing : IActivity<Processing>
+    {
+        public Processing() { }
+
+        public void Execute()
+        {
+            Console.WriteLine("Processing...");
+        }
+    }
+
 }
